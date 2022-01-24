d7adbdd06731d2b39866b9bfbfb45410a8170162
trippdev.eth <shriyesh15@gmail.com>
2022-01-24 13:48:09 +0530

    Worked on deployer script

--- migrations/3_deploy_scripts.js
+++ migrations/3_deploy_scripts.js
@@ -1,8 +1,11 @@
 const MultiSigWallet = artifacts.require("MultiSigWallet")
-
+async function deployDiamond() {
 module.exports = function (deployer, network, accounts) {
   const owners = accounts.slice(0, 3)
   const numConfirmationsRequired = 2
-
+  const accounts = await ethers.getSigners()
   deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired)
+  const contractOwner = await accounts[0]
+  console.log(`contractOwner ${contractOwner.address}`)
+}
 }
